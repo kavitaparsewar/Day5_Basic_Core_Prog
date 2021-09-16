cc4c72445dab692d5f99c9d918be346923ba6e21
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-17 02:10:37 +0530

    UC8 Even and odd number program added

---
+++ UC8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_assinment
+{
+    class UC8
+    {
+        public static void find_even_odd()
+        {
+            
+            Console.WriteLine("Enter a Number:- ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("This is Even Number.");
+                Console.Read();
+            }
+
+
+            else
+            {
+                Console.WriteLine("This is Odd Number.");
+                Console.Read();
+            }
+        }
+    }
+}
