b623282dd962090b0fd582d3f1cd61832ab89bdc
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-17 02:07:44 +0530

    UC6_Que & Remainder program added

---
+++ UC6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_assinment
+{
+    class UC6
+    {
+        public static void findQR()
+        {
+         	Console.Write("Enter first number:");
+			int num1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter second number:");
+			int num2 = Convert.ToInt32(Console.ReadLine());
+
+			int que = num1 / num2;
+			int remain = num1 % num2;
+
+			Console.WriteLine("Quotient is:" + que);
+			Console.WriteLine("Remainder is:" + remain);
+		}
+    }
+}
