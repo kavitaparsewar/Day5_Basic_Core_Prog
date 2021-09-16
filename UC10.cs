e7e304bf60558f519b4e8716af5d686f01f06a18
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-17 02:14:27 +0530

    UC10 Largest among three numbers program added

---
+++ UC10.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_assinment
+{
+    class UC10
+    {
+        public static void Find_Large ()
+            {
+            Console.WriteLine("Enter three numbers : ");
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Enter First number : ");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number : ");
+            int second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter third number : ");
+            int third = int.Parse(Console.ReadLine());
+
+            if (first > second)
+            {
+                
+                if (first > third)
+                {
+                    Console.WriteLine("largest number : " + first);
+                }
+                else
+                {
+                    Console.WriteLine("largest number : " + third);
+                }
+            }
+            else
+            {
+                if (second > third)
+                {
+                    Console.WriteLine("largest number : " + second);
+                }
+                else
+                {
+                    Console.WriteLine("largest number : " + third);
+                }
+            }
+
+
+        }
+    }
+}
