b07ab2c2749e4cfe71339e75467d8282cb165069
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-17 02:09:28 +0530

    UC7 swap two numbers program added

---
+++ UC7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_assinment
+{
+    class UC7
+    {
+        public static void swap_num()
+            {
+            Console.WriteLine("Enter the two numbers for swapping: ");
+            Console.WriteLine("Enter First Number : ");
+
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter Second Number :");
+
+            int num2 = int.Parse(Console.ReadLine());
+            int temp = num1;
+
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("\nAfter Swapping numbers are : ");
+            Console.WriteLine("\nFirst Number : " + num1);
+            Console.WriteLine("\nSecond Number : " + num2);
+            Console.Read();
+        }
+
+    }
+}
