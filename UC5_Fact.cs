cbcc2b81bdad537e48ae16d1dcc93d4232e5d670
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-18 19:56:05 +0530

    UC5 added

---
+++ UC5_Fact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_assinment
+{
+    class UC5_Fact
+    {
+        public static void find_factor()
+        {
+
+           
+            Console.WriteLine("Enter the Number:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                  
+                    Console.WriteLine(+i);
+                }
+            }
+           
+
+        }
+
+    }
+}
