5ea3be614bf5c90f16060cada5e0b65cd9d79614
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-18 19:53:40 +0530

    UC3 added

---
+++ UC3_Power2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_assinment
+{
+    class UC3_Power2
+    {
+        public static void Powerof2()
+        {
+
+         
+            Console.WriteLine("Enter number");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("Calculate power of 2 program");
+            if (n < 32)
+            {
+
+                Console.WriteLine(n.ToString() + "  to power of 2" + "=" + Math.Pow(2, n));
+
+            }
+        }
+
+    }
+
+  }
+
