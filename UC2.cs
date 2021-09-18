44e55fad423cc864ab5fa3c27d6f9337c831125b
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-18 19:52:47 +0530

    UC2 Leap year added

---
+++ UC2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_assinment
+{
+    class UC2
+    {
+        public static void Leap()
+        {
+            
+            Console.WriteLine("Enter year : ");
+
+            int year = Convert.ToInt32(Console.ReadLine());
+            
+            if (year % 4 == 0)
+                Console.WriteLine( year + " is leap year");
+            else
+                Console.WriteLine( year + " is not leap year");
+
+        }
+        
+    }
+
+ }
+
