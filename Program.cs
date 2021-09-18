b586c446499034876d792b595113535dc4bb6f3b
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-18 19:57:31 +0530

    Main program i.e Program.cs added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day5_assinment
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //UC1_Head_Tail.Head_tail();
+            //UC2.Leap();
+            //UC3_Power2.Powerof2();
+            UC5_Fact.find_factor();
+            //UC6.findQR();
+            //UC7.swap_num();
+            
+            //UC8.find_even_odd();
+            //UC9Vo_Co.find_vowel_conso();
+            //UC10.Find_Large();
+        }
+    }
+}
